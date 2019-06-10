5c76b9e621293149f24d813515e0d59ceaa8b004
njkf <43401909+njkf@users.noreply.github.com>
2019-06-10 15:52:38 +0200

    Add files via upload

---
+++ Hyperball.php
@@ -0,0 +1,20 @@
+<?php
+
+    
+class Hyperball extends Ball
+    {
+	public $name;
+    	public $level;
+
+        public function __construct ()
+        {
+            $name = 'Hyperball';
+            $level = 50;
+
+            parent::__construct($name, $level);
+        }
+
+    }
+
+
+?>
